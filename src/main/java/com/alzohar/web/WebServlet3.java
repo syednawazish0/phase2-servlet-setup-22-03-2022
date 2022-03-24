15b40f723e30254d36f7c5a6f7cee23e1fbf373d
Syed Nawazish Ali <syednawazisha@gmail.com>
2022-03-25 02:10:16 +0530

    adding phase2 servlet setup file

---
+++ src/main/java/com/alzohar/web/WebServlet3.java
@@ -0,0 +1,26 @@
+package com.alzohar.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebServlet3 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// set response content type
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		// prepare response body
+		out.print("<html><body>");
+		out.print("<h1>Hello, welcome to Http servlet program !</h1>");
+		out.print("</html></body>");
+
+		System.out.println(" -- Servlet request processing is completed! ---");
+	}
+}
